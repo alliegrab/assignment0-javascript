1978e1f3d971c07618b0910896fbedb23bddb43d
Allie Grab <allie.grab@yahoo.com>
2023-12-22 16:41:41 -0600

    Hello World!

--- hello.js
+++ hello.js
@@ -1,6 +1,9 @@
-function hello() {
+function hello() { 
 	// TODO: change this string so that your program prints "Hello world!" 
-	return "Hello!";
+	return "Hello world!";
+
 }
  
 module.exports = hello;
+
+
